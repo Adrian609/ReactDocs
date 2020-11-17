11d1610767b3991332453104777fc554ccc8fa9a
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-16 20:49:11 -0500

    prettier setup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
 class Square extends React.Component {
   render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
+    return <button className="square">{/* TODO */}</button>
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square />
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X"
 
     return (
       <div>
@@ -39,7 +35,7 @@ class Board extends React.Component {
           {this.renderSquare(8)}
         </div>
       </div>
-    );
+    )
   }
 }
 
@@ -55,13 +51,10 @@ class Game extends React.Component {
           <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
+    )
   }
 }
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"))
