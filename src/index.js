5fe41598001adf5daab3133d64bbf88026b7282c
Greg Curl <greg.a.curl@gmail.com>
2020-11-16 21:17:09 -0500

    set State with 'X'

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
       return (
-        <button className="square">
-          {this.props.value}
+        <button
+            className="square" 
+            onClick={() => this.setState({value: 'X'})}
+        >
+          {this.state.value}
         </button>
       );
     }
