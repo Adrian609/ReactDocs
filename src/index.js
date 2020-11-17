719f3798252aa549fa8ebcf5dd1498f4be70bc0a
ChrisKnowsDev <chrisharley81@gmail.com>
2020-11-17 10:23:48 -0500

    state set in Board component to determine next player, X and O displayed on board

--- src/index.js
+++ src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from "react-dom"
 import "./index.css"
 
 // Square
-class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: null,
-    }
-  }
-
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    )
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  )
 }
 
 // Board
@@ -26,10 +17,23 @@ class Board extends React.Component {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? "X" : "O"
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext })
+  }
+
   renderSquare(i) {
-    return <Square value={i} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render() {
