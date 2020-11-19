b63d2819d617534a82d73c57a507b9240573e7be
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2020-11-19 17:12:43 -0600

    at Lifting State Up

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -5,16 +5,31 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button 
+            className="square" 
+            onClick={() => this.props.onClick()}
+        >
+            {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-      return <Square />;
+      return (
+        <Square 
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)} 
+        />
+      );
     }
   
     render() {
