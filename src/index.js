7387a7dc2b6ce9aeb9d312b1b93836263612bd96
Greg Curl <greg.a.curl@gmail.com>
2020-11-23 20:00:00 -0500

    game complete with history

--- src/index.js
+++ src/index.js
@@ -50,14 +50,16 @@ function Square(props) {
           history: [{
             squares: Array(9).fill(null),
           }],
+          stepNumber: 0,
           xIsNext: true,
         };
       }
 
-      handleClick(i) {
-        const history = this.state.history;
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
+        
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
@@ -66,13 +68,21 @@ function Square(props) {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -80,12 +90,12 @@ function Square(props) {
               'Go to move #' + move :
               'Go to game start';
             return (
-              <li>
+              <li key={move}>
                 <button onClick={() => this.jumpTo(move)}>{desc}</button>
               </li>
             );
           });
-          
+
         let status;
         if (winner) {
         status = 'Winner: ' + winner;
